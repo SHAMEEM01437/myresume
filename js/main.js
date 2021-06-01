61e7aefc1114a0b641a3700196526a4148cedf36
SHAMEEM01437 <85186542+SHAMEEM01437@users.noreply.github.com>
2021-06-02 00:09:05 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const sr = ScrollReveal({
+// 	origin: 'left',
+// 	duration: 2000,
+// 	distance: '80px',
+// 	reset:true
+// })
+// sr.reveal('.content-title',{delay: 200})
+// sr.reveal('.teg-line',{delay: 200})
+// sr.reveal('.about-title',{delay: 200})
